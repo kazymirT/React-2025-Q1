2d2f31b636a91e0b65d84b9dd01ea3480a837a03
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-02-02 20:33:04 +0200

    feat: add api

---
+++ src/Api/api.ts
@@ -0,0 +1,22 @@
+import { Character, Info } from 'rickmortyapi';
+
+export interface FetchCharactersResponse {
+  results: Character[];
+  info: Info<Character[]>;
+}
+
+export const fetchCharactersByName = async (
+  name: string
+): Promise<FetchCharactersResponse> => {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?name=${name}`
+  );
+
+  if (!response.ok) {
+    const errorData = (await response.json()) as { error: string };
+    throw new Error(errorData.error || 'Failed to fetch characters');
+  }
+
+  const data: FetchCharactersResponse = await response.json();
+  return data;
+};
