d8d1127fcbbcb5a97838361990035f7814ba24ec
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-02-02 20:33:51 +0200

    refactor: update NoResult component

--- src/components/NoResult/NoResult.tsx
+++ src/components/NoResult/NoResult.tsx
@@ -1,12 +1,13 @@
-import React, { type ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import styles from './NoResult.module.scss';
+import { NoResultProps } from './types';
 
-export default class NoResult extends React.Component {
+export default class NoResult extends React.Component<NoResultProps> {
+  static defaultProps = {
+    errorMessage: 'The search returned no results, please try again.',
+  };
+
   render(): ReactNode {
-    return (
-      <p className={styles['no-result']}>
-        The search returned no results, please try again.
-      </p>
-    );
+    return <p className={styles['no-result']}>{this.props.errorMessage}</p>;
   }
 }
