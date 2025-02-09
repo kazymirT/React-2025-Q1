9faeaa8f3f60f89bcb6bfc71786666b652776428
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-02-09 20:23:48 +0200

    test: add test for component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,29 +2,29 @@ import { type FC } from 'react';
 import { CardProps } from './types';
 import styles from './Card.module.scss';
 import { NavLink, useSearchParams } from 'react-router-dom';
+import { CARD_TEST_ID } from './constants';
 
 const Card: FC<CardProps> = ({ data }) => {
   const { image, name, status, gender, created, id } = data;
   const [searchParams] = useSearchParams();
   return (
-    <NavLink to={`/details/${id}/?${searchParams}`} className={styles.card}>
-      <img
-        src={image || 'https://via.placeholder.com/300'}
-        alt={name ? `Image of ${name}` : 'Character image'}
-        width={300}
-        height={300}
-      />
+    <NavLink
+      to={`/details/${id}/?${searchParams}`}
+      className={styles.card}
+      data-testid={CARD_TEST_ID}
+    >
+      <img src={image} alt={`Image of ${name}`} width={300} height={300} />
       <div className={styles.content}>
-        <h2>{name || 'Unknown Name'}</h2>
+        <h2>{name}</h2>
         <ul>
           <li>
-            <strong>Status:</strong> {status || 'Unknown'}
+            <strong>Status:</strong> {status}
           </li>
           <li>
-            <strong>Gender:</strong> {gender || 'Unknown'}
+            <strong>Gender:</strong> {gender}
           </li>
           <li>
-            <strong>Created:</strong> {created || 'N/A'}
+            <strong>Created:</strong> {created}
           </li>
         </ul>
       </div>
