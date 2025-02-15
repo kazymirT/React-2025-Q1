ecccb384ee2463014597dde5c91b95e46a13a7b5
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-02-15 10:20:59 +0200

    refactor: update characterApi

--- src/redux/services/character.ts
+++ src/redux/services/character.ts
@@ -1,6 +1,7 @@
 import { Character } from 'rickmortyapi';
 import { baseApi } from './baseApi';
 import type { AllCharactersFetchArgs, CharactersResponse } from './types';
+import { setCharacters, setDetails } from '../slices/charactersSlice';
 
 export const characterApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -8,12 +9,28 @@ export const characterApi = baseApi.injectEndpoints({
       query: (id) => ({
         url: `/character/${id}`,
       }),
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(setDetails(data));
+        } catch (error) {
+          console.error(error);
+        }
+      },
     }),
     getAllCharacters: builder.query<CharactersResponse, AllCharactersFetchArgs>(
       {
         query: ({ name, page }) => ({
           url: `/character/?page=${page}&name=${name}`,
         }),
+        async onQueryStarted(_, { dispatch, queryFulfilled }) {
+          try {
+            const { data } = await queryFulfilled;
+            dispatch(setCharacters(data.results));
+          } catch (error) {
+            console.error(error);
+          }
+        },
       }
     ),
   }),
--- src/redux/slices/charactersSlice.ts
+++ src/redux/slices/charactersSlice.ts
@@ -17,15 +17,15 @@ export const charactersSlice = createSlice({
   name: 'character',
   initialState,
   reducers: {
-    addCharacters: (state, action: PayloadAction<Character[]>) => {
+    setCharacters: (state, action: PayloadAction<Character[]>) => {
       state.allCharacters = action.payload;
     },
-    addDetails: (state, action: PayloadAction<Character | null>) => {
+    setDetails: (state, action: PayloadAction<Character | null>) => {
       state.detailsCharacter = action.payload;
     },
   },
 });
 
-export const { addCharacters, addDetails } = charactersSlice.actions;
+export const { setCharacters, setDetails } = charactersSlice.actions;
 export const characters = (state: RootState) => state.characters;
 export default charactersSlice.reducer;
