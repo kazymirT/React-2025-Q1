313a1e7b7824c25b4e68efd2964880d070269bcb
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-02-16 20:42:57 +0200

    fix: fixed for rtk query

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,8 @@
 import { server } from '@/test/mock/server';
 import '@testing-library/jest-dom/vitest';
+import nodeFetch, { Request, Response } from 'node-fetch';
 
+Object.assign(global, { fetch: nodeFetch, Request, Response });
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 
 afterEach(() => server.resetHandlers());
